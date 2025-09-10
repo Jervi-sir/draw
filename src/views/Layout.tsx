345b20ac46f860567e47e87193c49670d2d426af
Jervi-sir <gacembekhira@gmail.com>
2025-09-10 21:03:21 +0100

    made the setup

--- src/views/Layout.tsx
+++ src/views/Layout.tsx
@@ -3,10 +3,10 @@ import { Outlet } from "@tanstack/react-router";
 
 export default function Layout() {
   return (
-    <div className="flex h-full w-full flex-col p-1">
-      <Navbar />
-      <div className="h-full w-full p-3 pt-1">
-        <div className="flex h-full flex-row justify-center gap-8 overflow-clip rounded-xl border-2 border-black bg-gray-200/60 dark:border-white dark:bg-gray-900">
+    <div className="flex h-full w-full flex-col p-3">
+      {/* <Navbar /> */}
+      <div className="h-full w-full p-0">
+        <div className="flex h-full flex-row justify-center gap-8 overflow-clip rounded-xl border-black bg-gray-200/60 dark:border-white dark:bg-gray-900">
           <Outlet />
         </div>
       </div>
